ac4e51197a34204036cb9a00a67c6692f52b1f0d
TomozQ <inf.4clover@gmail.com>
2022-10-06 22:43:34 +0900

    display storybook

---
+++ stories/StyledButton.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentMeta } from '@storybook/react'
+import { StyledButton } from '../components/StyledButton'
+
+// ファイル内のStoryの設定（メタデータオブジェクト）
+export default {
+    // グループ名
+    title: 'StyledButton',
+    // 使用するコンポーネント
+    component: StyledButton,
+} as ComponentMeta<typeof StyledButton>
+
+export const Primary = (props: any) => {
+    return (
+        <StyledButton { ...props } variant='primary'>
+            Primary
+        </StyledButton>
+    )
+}
+export const Success = (props: any) => {
+    return (
+        <StyledButton { ...props } variant='success'>
+            Success
+        </StyledButton>
+    )
+}
+export const Transparent = (props: any) => {
+    return (
+        <StyledButton { ...props } variant='transparent'>
+            Transparent
+        </StyledButton>
+    )
+}
