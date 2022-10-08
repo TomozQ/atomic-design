17b23582c5e1d68088937e1205b3a9fd8c296d16
TomozQ <inf.4clover@gmail.com>
2022-10-08 17:03:46 +0900

    display span text content test

---
+++ components/DelayInput/index.spec.tsx
@@ -0,0 +1,40 @@
+/*
+    テスト内容
+    ・初期の表示は空である
+    ・入力直後は「入力中」と表示される
+    ・入力して1秒経過した後に入力した内容が表示される
+    ・入力して1秒経過した後にonChangeコールバックが呼ばれる
+*/
+
+import { render, screen, RenderResult } from '@testing-library/react'
+import { DelayInput } from './index'
+
+// DelayInputコンポーネントに関するテスト
+describe('DelayInput', () => {
+    let renderResult: RenderResult
+    let handleChange: jest.Mock
+
+    beforeEach(() => {
+        // モック関数を作成する
+        handleChange = jest.fn()
+        /*
+            jest.fn()
+            モック関数を作成する関数 コールバックが呼ばれたかなどテストできる
+        */
+
+        // モック関数をDelayInputに渡して描画
+        renderResult = render(<DelayInput onChange={ handleChange } />)
+    })
+
+    afterEach(() => {
+        renderResult.unmount()
+    })
+
+    // span要素のテキストが空であることをテスト
+    it('should display empty in span on initial render', () => {
+        const spanNode = screen.getByTestId('display-text') as HTMLSpanElement
+
+        // 初期表示は空
+        expect(spanNode).toHaveTextContent('入力したテキスト')
+    })
+})
